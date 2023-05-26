832442bee98ab80f5283e80b0d45377b688cb74c
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2023-05-26 19:20:38 +0300

    add base fragment class

--- app/src/main/java/com/cupcake/jobsfinder/ui/base/BaseFragment.kt
+++ app/src/main/java/com/cupcake/jobsfinder/ui/base/BaseFragment.kt
@@ -1,4 +1,51 @@
 package com.cupcake.jobsfinder.ui.base
 
-abstract class BaseFragment {
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+
+
+abstract class BaseFragment<DB: ViewDataBinding, VM: BaseViewModel>(
+    @LayoutRes private val layoutId: Int,
+    private val viewModelClass: Class<VM>
+): Fragment() {
+
+    abstract val LOG_TAG: String
+
+    abstract fun onInitDataBinding()
+
+    lateinit var viewModel: VM
+
+    private var _binding: DB? = null
+    protected val binding get() = _binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activity.let {
+            viewModel = ViewModelProvider(it!!)[viewModelClass]
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        onInitDataBinding()
+        _binding?.lifecycleOwner = viewLifecycleOwner
+        return binding?.root
+
+    }
+
+    protected fun log(value: String) {
+        Log.v(LOG_TAG, value)
+    }
+
 }
