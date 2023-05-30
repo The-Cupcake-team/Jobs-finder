ec1fc260852556a07a8b52e027b17cccceb1b7c4
ali mohamed ali <aliabuhussien371@gmail.com>
2023-05-30 21:00:24 +0300

    add getAllJobTitle in repository and RepositoryImpl

--- app/src/main/java/com/cupcake/jobsfinder/data/repository/Repository.kt
+++ app/src/main/java/com/cupcake/jobsfinder/data/repository/Repository.kt
@@ -1,4 +1,7 @@
 package com.cupcake.jobsfinder.data.repository
 
+import com.cupcake.jobsfinder.data.model.JobTitleDto
+
 interface Repository {
+    suspend fun JobTitle():List<JobTitleDto>
 }
--- app/src/main/java/com/cupcake/jobsfinder/data/repository/RepositoryImpl.kt
+++ app/src/main/java/com/cupcake/jobsfinder/data/repository/RepositoryImpl.kt
@@ -1,3 +1,21 @@
 package com.cupcake.jobsfinder.data.repository
 
-class RepositoryImpl : Repository
+import com.cupcake.jobsfinder.data.model.JobTitleDto
+
+class RepositoryImpl : Repository {
+    override suspend fun JobTitle(): List<JobTitleDto> {
+        return listOf(
+            JobTitleDto(
+                id = 0,
+                idJobTitle = 0,
+                companyName = "ali and hasan",
+                createdAt = "2015-5-1",
+                workType = "full time",
+                location = "remote",
+                jobType = "full time",
+                jobDescription = "android",
+                salary = 1000.00
+            )
+        )
+    }
+}
