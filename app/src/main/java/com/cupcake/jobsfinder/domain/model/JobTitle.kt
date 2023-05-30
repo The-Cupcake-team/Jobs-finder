32f54c1bb5f03f9cc2f598bed7d2eb66aa90bab0
ali mohamed ali <aliabuhussien371@gmail.com>
2023-05-30 21:44:42 +0300

    Add parameters to JobTitle model

--- app/src/main/java/com/cupcake/jobsfinder/domain/model/JobTitle.kt
+++ app/src/main/java/com/cupcake/jobsfinder/domain/model/JobTitle.kt
@@ -1,3 +1,10 @@
 package com.cupcake.jobsfinder.domain.model
 
-data class JobTitle()
+data class JobTitle(
+    val companyName: String,
+    val workType: String,
+    val location: String,
+    val jobType: String,
+    val jobDescription: String,
+    val salary : Double
+)
