c2f395646b2f6b35180a4258fef785246f9a0033
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2023-05-30 21:20:23 +0300

    add id field to PostDto class and edit createdAt field to be Long stand of string

--- app/src/main/java/com/cupcake/jobsfinder/data/remote/modle/PostDto.kt
+++ app/src/main/java/com/cupcake/jobsfinder/data/remote/modle/PostDto.kt
@@ -1,9 +1,10 @@
 package com.cupcake.jobsfinder.data.remote.modle
 
 data class PostDto(
+    val id: Long,
     val username: String,
     val jobTitle: String,
-    val postingTime: String,
+    val createdAt: Long,
     val description: String,
     val imageUrl: String,
     val likesCount: Int,
