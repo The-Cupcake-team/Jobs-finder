8b990071475c9e0d0bd954b4150808ce4b28d437
Hasa9nAli <hasan.20cs75@student.uomosul.edu.iq>
2023-05-31 15:23:19 +0300

    refactoring the parameter of  JobTitleDto

--- app/src/main/java/com/cupcake/jobsfinder/data/dto/JobTitleDto.kt
+++ app/src/main/java/com/cupcake/jobsfinder/data/dto/JobTitleDto.kt
@@ -2,12 +2,5 @@ package com.cupcake.jobsfinder.data.dto
 
 data class JobTitleDto(
     val id: Long,
-    val idJobTitle: Long,
-    val companyName: String,
-    val createdAt: String,
-    val workType: String,
-    val location: String,
-    val jobType: String,
-    val jobDescription: String,
-    val salary : Double
+    val title : String
     )
