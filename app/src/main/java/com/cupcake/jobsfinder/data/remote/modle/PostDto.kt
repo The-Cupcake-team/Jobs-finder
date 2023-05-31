12cdc833ea59c6f1503a395154537b14b1366378
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2023-05-31 15:36:20 +0300

    edit post id data type to be string stand of long

--- app/src/main/java/com/cupcake/jobsfinder/data/remote/modle/PostDto.kt
+++ app/src/main/java/com/cupcake/jobsfinder/data/remote/modle/PostDto.kt
@@ -1,12 +1,12 @@
 package com.cupcake.jobsfinder.data.remote.modle
 
 data class PostDto(
-    val id: Long,
+    val id: String,
     val username: String,
     val jobTitle: String,
     val createdAt: Long,
     val description: String,
     val imageUrl: String,
     val likesCount: Int,
-    val commentCount: Int
+    val commentsCount: Int
 )
