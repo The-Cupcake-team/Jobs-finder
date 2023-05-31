a8192bc808bedd67058e8adfab205827264d82d6
mustafa-sab3alarab <mustafa777alarab@gmail.com>
2023-05-31 20:15:48 +0200

    take limit of GetAllJobUseCase

--- app/src/main/java/com/cupcake/jobsfinder/domain/usecase/GetAllJobUseCase.kt
+++ app/src/main/java/com/cupcake/jobsfinder/domain/usecase/GetAllJobUseCase.kt
@@ -1,8 +1,9 @@
 package com.cupcake.jobsfinder.domain.usecase
 
 import com.cupcake.jobsfinder.data.repository.Repository
+import kotlinx.coroutines.flow.take
 import javax.inject.Inject
 
 class GetAllJobUseCase @Inject constructor(private val repository: Repository) {
-    suspend operator fun invoke(limit: Int) = repository.getAllJobs()
+    suspend operator fun invoke(limit: Int) = repository.getAllJobs().take(limit)
 }
