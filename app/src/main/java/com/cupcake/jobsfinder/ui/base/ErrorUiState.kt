3817ff587ee1f4772228d0559d248497ae1b92f5
mustafa-sab3alarab <mustafa777alarab@gmail.com>
2023-05-31 20:16:55 +0200

    create jobs ui state model

---
+++ app/src/main/java/com/cupcake/jobsfinder/ui/base/ErrorUiState.kt
@@ -0,0 +1,6 @@
+package com.cupcake.jobsfinder.ui.base
+
+data class ErrorUiState(
+    val code : Int,
+    val message : String
+)
---
+++ app/src/main/java/com/cupcake/jobsfinder/ui/jobs/JobsUiState.kt
@@ -0,0 +1,20 @@
+package com.cupcake.jobsfinder.ui.jobs
+
+import com.cupcake.jobsfinder.ui.base.ErrorUiState
+
+
+data class JobsUiState(
+    val jobs: List<JobUiState> = emptyList(),
+    val isLoading: Boolean = false,
+    val error: List<ErrorUiState> = emptyList()
+)
+
+
+data class JobUiState(
+    val image: String = "",
+    val title: String = "",
+    val companyName: String = "",
+    val detailsChip: List<String> = emptyList(),
+    val location: String = "",
+    val salary: String = ""
+)
