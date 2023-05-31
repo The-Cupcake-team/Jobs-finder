38ffca9f117e424a365e5597d691530b48c5f6f4
Hassan Ayman Ahmed <hassan3ayman@gmail.com>
2023-05-31 16:58:11 +0200

    make getAllJobs in repository

--- app/src/main/java/com/cupcake/jobsfinder/data/repository/Repository.kt
+++ app/src/main/java/com/cupcake/jobsfinder/data/repository/Repository.kt
@@ -1,4 +1,8 @@
 package com.cupcake.jobsfinder.data.repository
 
+import com.cupcake.jobsfinder.domain.model.Job
+import kotlinx.coroutines.flow.Flow
+
 interface Repository {
+    suspend fun getAllJobs(): Flow<List<Job>>
 }
--- app/src/main/java/com/cupcake/jobsfinder/data/repository/RepositoryImpl.kt
+++ app/src/main/java/com/cupcake/jobsfinder/data/repository/RepositoryImpl.kt
@@ -1,3 +1,25 @@
 package com.cupcake.jobsfinder.data.repository
 
-class RepositoryImpl : Repository
+import android.util.Log
+import com.cupcake.jobsfinder.data.remote.JobApiService
+import com.cupcake.jobsfinder.domain.mapper.toJob
+import com.cupcake.jobsfinder.domain.model.Job
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class RepositoryImpl @Inject constructor(private val api: JobApiService) : Repository {
+    override suspend fun getAllJobs(): Flow<List<Job>> {
+        return flow {
+            val response = api.getAllJobs()
+            if (response.isSuccessful) {
+                response.body()?.value?.map { it.toJob() }?.let { emit(it) }
+            } else {
+                Log.v("repositoryImp", response.message())
+            }
+        }.flowOn(Dispatchers.IO)
+    }
+
+}
