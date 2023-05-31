511c23bb79a7cdcd111b078a772758470d602dce
Hassan Ayman Ahmed <hassan3ayman@gmail.com>
2023-05-31 16:56:50 +0200

    add getAllJobs api call

--- app/src/main/java/com/cupcake/jobsfinder/data/remote/JobApiService.kt
+++ app/src/main/java/com/cupcake/jobsfinder/data/remote/JobApiService.kt
@@ -1,4 +1,10 @@
 package com.cupcake.jobsfinder.data.remote
 
+import com.cupcake.jobsfinder.data.remote.response.job.JobDto
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface JobApiService {
+    @GET("/job")
+    suspend fun getAllJobs(): Response<BaseResponse<JobDto>>
 }
