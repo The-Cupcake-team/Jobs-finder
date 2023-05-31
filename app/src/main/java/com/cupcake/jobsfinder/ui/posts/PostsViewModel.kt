b477e486b3e0351933b5e5c0798a8bea7e77f1fa
David samuel <davidsamuelramsis@gmail.com>
2023-05-31 15:53:50 +0300

    add val ui state in view model

--- app/src/main/java/com/cupcake/jobsfinder/ui/posts/PostsViewModel.kt
+++ app/src/main/java/com/cupcake/jobsfinder/ui/posts/PostsViewModel.kt
@@ -2,11 +2,14 @@ package com.cupcake.jobsfinder.ui.posts
 
 import com.cupcake.jobsfinder.domain.usecase.GetPostsUseCase
 import com.cupcake.jobsfinder.ui.base.BaseViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 class PostsViewModel @Inject constructor(
     private val getPostsUseCase: GetPostsUseCase
 ): BaseViewModel() {
 
-
+    private val _uiState = MutableStateFlow(PostsUIState())
+    val uiState: StateFlow<PostsUIState> = _uiState
 }
