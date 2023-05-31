21df268f954fa1f4306045f80da66fb2a589aca4
ali mohamed ali <aliabuhussien371@gmail.com>
2023-05-31 15:31:17 +0300

    refactoring the parameter of getAllJobTitles

--- app/src/main/java/com/cupcake/jobsfinder/data/repository/RepositoryImpl.kt
+++ app/src/main/java/com/cupcake/jobsfinder/data/repository/RepositoryImpl.kt
@@ -7,14 +7,7 @@ class RepositoryImpl : Repository {
         return listOf(
             JobTitleDto(
                 id = 0,
-                idJobTitle = 0,
-                companyName = "ali and hasan",
-                createdAt = "2015-5-1",
-                workType = "full time",
-                location = "remote",
-                jobType = "full time",
-                jobDescription = "android",
-                salary = 1000.00
+                title = "Android"
             )
         )
     }
