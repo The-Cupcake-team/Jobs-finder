b96ca2b87a51c56fc89fa16f8294581f4736e7ec
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2023-06-01 19:18:22 +0300

    change PostUiState data fields to the required

--- app/src/main/java/com/cupcake/jobsfinder/ui/posts/PostUIState.kt
+++ app/src/main/java/com/cupcake/jobsfinder/ui/posts/PostUIState.kt
@@ -8,22 +8,11 @@ data class PostsUIState(
 
 data class PostItemUIState(
     val id: String,
-    val postInteraction: PostInteraction,
-    val username: String = "",
-    val jobTitle: String = "",
     val createdAt: Long,
     val description: String,
-    val imageUrl: String = "",
-    val likesCount: Int = 0,
-    val commentCount: Int = 0
 )
 
-data class PostInteraction(
-    val isFollowed: Boolean = false,
-    val isLiked: Boolean = false,
-    val isSaved: Boolean = false,
-)
 
 data class ErrorUIState(
-    val message: String
+    val message: String = "UNKNOWN ERROR"
 )
