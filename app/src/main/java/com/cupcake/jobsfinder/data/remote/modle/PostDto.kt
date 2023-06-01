8d67d76900c8c0a4d0c6ca9753f6eec0f15b6d19
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2023-06-01 18:17:20 +0300

    improve PostDto class to contain id,createdAt and description

--- app/src/main/java/com/cupcake/jobsfinder/data/remote/modle/PostDto.kt
+++ app/src/main/java/com/cupcake/jobsfinder/data/remote/modle/PostDto.kt
@@ -2,11 +2,6 @@ package com.cupcake.jobsfinder.data.remote.modle
 
 data class PostDto(
     val id: String,
-    val username: String,
-    val jobTitle: String,
     val createdAt: Long,
     val description: String,
-    val imageUrl: String,
-    val likesCount: Int,
-    val commentsCount: Int
 )
