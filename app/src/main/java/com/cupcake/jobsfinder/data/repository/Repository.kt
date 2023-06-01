4f627fc9c8da21f42f892fc48ffff2be86bb54fa
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2023-06-01 18:46:58 +0300

    improve getAllPosts function to return flow of postDto

--- app/src/main/java/com/cupcake/jobsfinder/data/repository/Repository.kt
+++ app/src/main/java/com/cupcake/jobsfinder/data/repository/Repository.kt
@@ -1,8 +1,9 @@
 package com.cupcake.jobsfinder.data.repository
 
 import com.cupcake.jobsfinder.data.remote.modle.PostDto
+import kotlinx.coroutines.flow.Flow
 
 interface Repository {
 
-    suspend fun getAllPosts(): List<PostDto>
+    suspend fun getAllPosts(): Flow<List<PostDto>>
 }
--- app/src/main/java/com/cupcake/jobsfinder/data/repository/RepositoryImpl.kt
+++ app/src/main/java/com/cupcake/jobsfinder/data/repository/RepositoryImpl.kt
@@ -1,47 +1,37 @@
 package com.cupcake.jobsfinder.data.repository
 
 import com.cupcake.jobsfinder.data.remote.modle.PostDto
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class RepositoryImpl @Inject constructor(): Repository {
-    override suspend fun getAllPosts(): List<PostDto> {
+    override suspend fun getAllPosts(): Flow<List<PostDto>> {
         return fakePosts() //todo:[ call getAllPosts from JopApiService]
     }
 
-    private fun fakePosts(): List<PostDto> {
-        return listOf(
-            PostDto(
-                "1",
-                "bilal",
-                "android developer",
-                9992453L,
-                "bla bla 1",
-                "https://blabla.com",
-                10,
-                9
-            ),
-            PostDto(
-                "2",
-                "david",
-                "android developer",
-                662453L,
-                "bla bla 2",
-                "https://david.com",
-                1000,
-                90
-            ),
-            PostDto(
-                "3",
-                "ameer",
-                "android developer",
-                855453L,
-                "bla bla 3",
-                "https://david.com",
-                10000,
-                900
-            ),
-
-
-        )
+    private fun fakePosts(): Flow<List<PostDto>> {
+        return flow<List<PostDto>> {
+            listOf(
+                PostDto(
+                    "1",
+                    9992453L,
+                    "android developer",
+                ),
+                PostDto(
+                    "1",
+                    9992453L,
+                    "android developer",
+                ),
+                PostDto(
+                    "1",
+                    9992453L,
+                    "android developer",
+                )
+            )
+        }.flowOn(Dispatchers.IO)
     }
 }
