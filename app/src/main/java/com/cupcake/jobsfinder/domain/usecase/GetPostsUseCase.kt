0566152f66354efe92713e02d7284de6e5d564d3
David samuel <davidsamuelramsis@gmail.com>
2023-06-01 18:55:09 +0300

    use return to flow in use case

--- app/src/main/java/com/cupcake/jobsfinder/domain/usecase/GetPostsUseCase.kt
+++ app/src/main/java/com/cupcake/jobsfinder/domain/usecase/GetPostsUseCase.kt
@@ -3,13 +3,16 @@ package com.cupcake.jobsfinder.domain.usecase
 import com.cupcake.jobsfinder.data.repository.Repository
 import com.cupcake.jobsfinder.domain.mapper.toPost
 import com.cupcake.jobsfinder.domain.model.Post
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetPostsUseCase @Inject constructor(
     private val repository: Repository
 ) {
-    suspend operator fun invoke(): List<Post> {
-        return repository.getAllPosts().map { it.toPost() }
-            .sortedByDescending { it.createdAt }
+    suspend operator fun invoke(): Flow<List<Post>> {
+        return repository.getAllPosts().map { postDto ->
+            postDto.map { it.toPost() }
+        }
     }
 }
