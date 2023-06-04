cce1f843f11f887ffce12cffda96a83c7ebf620a
Hasa9nAli <hasan.20cs75@student.uomosul.edu.iq>
2023-06-04 13:14:41 +0300

    add provider of RepositoryImp

--- app/src/main/java/com/cupcake/jobsfinder/di/RepositoryModule.kt
+++ app/src/main/java/com/cupcake/jobsfinder/di/RepositoryModule.kt
@@ -1,15 +1,28 @@
 package com.cupcake.jobsfinder.di
 
+import com.cupcake.jobsfinder.data.remote.JobApiService
 import com.cupcake.jobsfinder.data.repository.Repository
 import com.cupcake.jobsfinder.data.repository.RepositoryImpl
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.SerialInfo
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
     @Binds
     abstract fun bindsRepository(repository: RepositoryImpl): Repository
+
+    companion object {
+        @JvmStatic
+        @Provides
+        fun provideRepository(findJobApi: JobApiService): RepositoryImpl {
+            return RepositoryImpl(findJobApi)
+        }
+    }
+
 }
