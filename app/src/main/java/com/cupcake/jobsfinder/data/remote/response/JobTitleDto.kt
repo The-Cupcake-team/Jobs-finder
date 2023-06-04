d8b2968bc98b676b7881a8f76f9ff874c4795823
Hasa9nAli <hasan.20cs75@student.uomosul.edu.iq>
2023-06-04 09:03:04 +0300

    change the Serializable to SerializedName

--- app/src/main/java/com/cupcake/jobsfinder/data/remote/response/JobTitleDto.kt
+++ app/src/main/java/com/cupcake/jobsfinder/data/remote/response/JobTitleDto.kt
@@ -1,9 +1,8 @@
 package com.cupcake.jobsfinder.data.remote.response
 
-import kotlinx.serialization.Serializable
+import com.google.gson.annotations.SerializedName
 
-@Serializable
 data class JobTitleDto(
-    val id: String,
-    val title : String
+   @SerializedName("id") val id: String,
+   @SerializedName("title") val title : String
 )
