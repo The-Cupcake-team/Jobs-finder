f90aac2e16affa055b8c5a095b04379d4b67fa01
Hasa9nAli <hasan.20cs75@student.uomosul.edu.iq>
2023-06-04 12:05:38 +0300

    add logic to the GetAllJobTitleUseCase

--- app/src/main/java/com/cupcake/jobsfinder/domain/usecase/GetAllJobTitleUseCase.kt
+++ app/src/main/java/com/cupcake/jobsfinder/domain/usecase/GetAllJobTitleUseCase.kt
@@ -2,15 +2,15 @@ package com.cupcake.jobsfinder.domain.usecase
 
 import com.cupcake.jobsfinder.data.repository.Repository
 import com.cupcake.jobsfinder.domain.mapper.toJobTitle
-import com.cupcake.jobsfinder.domain.model.JobTitle
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetAllJobTitleUseCase @Inject constructor(
     private val jobTitleRepository: Repository,
 ) {
-    suspend operator fun invoke(): List<JobTitle> {
-        return jobTitleRepository.getAllJobTitles().map { jobTitleDto ->
-            jobTitleDto.toJobTitle()
-        }
+    suspend operator fun invoke()
+    =  jobTitleRepository.getAllJobTitles()
+        .map { listOfJobTitleDto -> listOfJobTitleDto.map { jobTitleDto ->
+            jobTitleDto.toJobTitle() } }
+
     }
-}
