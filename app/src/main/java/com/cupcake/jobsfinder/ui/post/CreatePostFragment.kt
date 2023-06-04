61ca3f5442953b06cb95e315fb7d6d34f25dbfd8
Abdelrahman Sedky <46361405+Abdelrahman-Sedky@users.noreply.github.com>
2023-06-04 03:13:05 +0300

    delete unused imports

--- app/src/main/java/com/cupcake/jobsfinder/ui/post/CreatePostFragment.kt
+++ app/src/main/java/com/cupcake/jobsfinder/ui/post/CreatePostFragment.kt
@@ -1,10 +1,5 @@
 package com.cupcake.jobsfinder.ui.post
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.cupcake.jobsfinder.R
 import com.cupcake.jobsfinder.databinding.FragmentCreatePostBinding
 import com.cupcake.jobsfinder.ui.base.BaseFragment
