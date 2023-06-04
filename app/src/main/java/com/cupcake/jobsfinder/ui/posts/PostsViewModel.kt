67215081d6da3f703e360586be2ff6940814e9d1
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2023-06-04 16:59:55 +0300

    refactor uiState and catch exception using CoroutineExceptionHandler

--- app/src/main/java/com/cupcake/jobsfinder/ui/posts/PostsViewModel.kt
+++ app/src/main/java/com/cupcake/jobsfinder/ui/posts/PostsViewModel.kt
@@ -2,12 +2,12 @@ package com.cupcake.jobsfinder.ui.posts
 
 import androidx.lifecycle.viewModelScope
 import com.cupcake.jobsfinder.domain.model.Post
-import com.cupcake.jobsfinder.domain.useCase.GetPostsUseCase
+import com.cupcake.jobsfinder.domain.usecase.GetPostsUseCase
 import com.cupcake.jobsfinder.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -22,6 +22,10 @@ class PostsViewModel @Inject constructor(
 
     private val _errors: MutableList<ErrorUIState> = mutableListOf()
 
+    val handler = CoroutineExceptionHandler { _, exception ->
+        onGetPostsFailure(exception)
+    }
+
     init {
         onGetPosts()
     }
@@ -29,25 +33,28 @@ class PostsViewModel @Inject constructor(
     private fun onGetPosts() {
         _uiState.update { it.copy(isLoading = true) }
 
-        viewModelScope.launch {
-            getPostsUseCase().catch { throwable ->
-                _uiState.update {
-                    _errors.add(ErrorUIState(throwable.message.toString()))
-                    it.copy(errors = _errors)
-                }
-            }.collect { posts ->
-                _uiState.update {
-                    it.copy(
-                        isLoading = false,
-                        postsResult = posts.map { post -> post.toPostItemUIState() })
-                }
+        viewModelScope.launch(handler) {
+            getPostsUseCase().collect { posts ->
+                onGetPostsSuccess(posts)
             }
-
-
         }
 
     }
 
+    private fun onGetPostsSuccess(posts: List<Post>){
+        _uiState.update {
+            it.copy(
+                isLoading = false,
+                postsResult = posts.map { post -> post.toPostItemUIState() })
+        }
+    }
+
+    private fun onGetPostsFailure(throwable: Throwable){
+        _uiState.update {
+            _errors.add(ErrorUIState(throwable.message.toString()))
+            it.copy(errors = _errors)
+        }
+    }
 
     suspend fun onInternetDisconnected() {
         _uiState.update { it.copy(isLoading = true) }
