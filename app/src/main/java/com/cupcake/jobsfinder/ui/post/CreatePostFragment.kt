0940bd4e4c946f58197b1deacc6828269f0a4133
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-06-05 19:50:56 +0300

    update fragment

--- app/src/main/java/com/cupcake/jobsfinder/ui/post/CreatePostFragment.kt
+++ app/src/main/java/com/cupcake/jobsfinder/ui/post/CreatePostFragment.kt
@@ -1,5 +1,7 @@
 package com.cupcake.jobsfinder.ui.post
 
+import android.os.Bundle
+import android.view.View
 import com.cupcake.jobsfinder.R
 import com.cupcake.jobsfinder.databinding.FragmentCreatePostBinding
 import com.cupcake.jobsfinder.ui.base.BaseFragment
@@ -9,4 +11,10 @@ class CreatePostFragment : BaseFragment<FragmentCreatePostBinding, CreatePostVie
     CreatePostViewModel::class.java
 ) {
     override val LOG_TAG: String = this.javaClass.name
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
+
+
