b8964223e6791d7468055078f7145259405fe67d
David samuel <davidsamuelramsis@gmail.com>
2023-06-05 21:30:43 +0300

    remove nullability in post and handle it in mapper

--- app/src/main/java/com/cupcake/jobsfinder/domain/model/Post.kt
+++ app/src/main/java/com/cupcake/jobsfinder/domain/model/Post.kt
@@ -1,7 +1,7 @@
 package com.cupcake.jobsfinder.domain.model
 
 data class Post(
-    val id: String?,
-    val createdAt: Long?,
-    val content: String?
+    val id: String,
+    val createdAt: Long,
+    val content: String
 )
