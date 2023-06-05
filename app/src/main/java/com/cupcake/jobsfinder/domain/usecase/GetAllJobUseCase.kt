e7642f6c549a6f0412125000c5a67acda1c70734
mustafa-sab3alarab <mustafa777alarab@gmail.com>
2023-06-05 16:46:51 +0200

    fix rename jobwithtitle issue

--- app/src/main/java/com/cupcake/jobsfinder/domain/usecase/GetAllJobUseCase.kt
+++ app/src/main/java/com/cupcake/jobsfinder/domain/usecase/GetAllJobUseCase.kt
@@ -2,13 +2,13 @@ package com.cupcake.jobsfinder.domain.useCase
 
 import com.cupcake.jobsfinder.domain.mapper.toJobWithJobTitle
 import com.cupcake.jobsfinder.domain.repository.Repository
-import com.cupcake.jobsfinder.domain.model.JobWithJobTitle
+import com.cupcake.jobsfinder.domain.model.JobWithTitle
 import javax.inject.Inject
 
 class GetAllJobUseCase @Inject constructor(
     private val repository: Repository
 ) {
-    suspend operator fun invoke(limit: Int): List<JobWithJobTitle> {
+    suspend operator fun invoke(limit: Int): List<JobWithTitle> {
         return repository.getJobs().take(limit).map { it.toJobWithJobTitle() }
     }
 }
