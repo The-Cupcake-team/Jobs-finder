93835ec80940fd5a5e904e6487c479d87fc6540a
TarekIdrees <tarekdrias321@gmail.com>
2023-06-06 23:55:17 +0300

    fix call issue

--- app/src/main/java/com/cupcake/jobsfinder/ui/create_job/CreateJobFormOneFragment.kt
+++ app/src/main/java/com/cupcake/jobsfinder/ui/create_job/CreateJobFormOneFragment.kt
@@ -1,10 +1,10 @@
 package com.cupcake.jobsfinder.ui.create_job
 
 import com.cupcake.jobsfinder.R
-import com.cupcake.jobsfinder.databinding.ItemCreateJobFormTwoBinding
+import com.cupcake.jobsfinder.databinding.ItemCreateJobFormOneBinding
 import com.cupcake.jobsfinder.ui.base.BaseFragment
 
-class CreateJobFormOneFragment : BaseFragment<ItemCreateJobFormTwoBinding, CreateJobViewModel>(
+class CreateJobFormOneFragment : BaseFragment<ItemCreateJobFormOneBinding, CreateJobViewModel>(
     R.layout.item_create_job_form_one, CreateJobViewModel::class.java
 ) {
     override val LOG_TAG: String = this.javaClass.name
