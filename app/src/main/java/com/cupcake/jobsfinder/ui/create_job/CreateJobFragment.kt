caf50e8b912d07db1e3cb1a3d70a4ddf48e2a113
TarekIdrees <tarekdrias321@gmail.com>
2023-06-06 13:25:47 +0300

    solve issue

--- app/src/main/java/com/cupcake/jobsfinder/ui/create_job/CreateJobFragment.kt
+++ app/src/main/java/com/cupcake/jobsfinder/ui/create_job/CreateJobFragment.kt
@@ -1,10 +1,10 @@
 package com.cupcake.jobsfinder.ui.create_job
 
 import com.cupcake.jobsfinder.R
-import com.cupcake.jobsfinder.databinding.FragmentCreatePostBinding
+import com.cupcake.jobsfinder.databinding.FragmentCreateJobBinding
 import com.cupcake.jobsfinder.ui.base.BaseFragment
 
-class CreateJobFragment : BaseFragment<FragmentCreatePostBinding, CreateJobViewModel>(
+class CreateJobFragment : BaseFragment<FragmentCreateJobBinding, CreateJobViewModel>(
     R.layout.fragment_create_job, CreateJobViewModel::class.java
 ) {
     override val LOG_TAG: String = this.javaClass.name
