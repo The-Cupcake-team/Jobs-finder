d256020e8c5b16de784595c09fbac1642a30031e
David samuel <davidsamuelramsis@gmail.com>
2023-06-06 17:19:51 +0300

    add showWhenLoading adapter

--- app/src/main/java/com/cupcake/jobsfinder/utils/BindingAdapter.kt
+++ app/src/main/java/com/cupcake/jobsfinder/utils/BindingAdapter.kt
@@ -1,5 +1,6 @@
 package com.cupcake.jobsfinder.utils
 
+import android.view.View
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.cupcake.jobsfinder.ui.base.BaseAdapter
@@ -9,4 +10,11 @@ fun <T> setRecyclerItems(view: RecyclerView, items: List<T>?) {
     items?.let {
         (view.adapter as BaseAdapter<T>).setData(items)
     } ?: (view.adapter as BaseAdapter<T>).setData(emptyList())
+}
+
+@BindingAdapter(value = ["app:showWhenLoading"])
+fun <T> showWhenLoading(view: View, state: Boolean?) {
+    state?.let {
+        view.visibility = if (state) View.VISIBLE else View.INVISIBLE
+    }
 }
