70a1ec82b86005bb3dbe64325f16a3dfa626fcaf
Hassan Ayman Ahmed <hassan3ayman@gmail.com>
2023-06-08 00:58:36 +0200

    add other list of jobs in jobsUiState & add mapper to jobsUiState

--- app/src/main/java/com/cupcake/jobsfinder/ui/jobs/JobsUiState.kt
+++ app/src/main/java/com/cupcake/jobsfinder/ui/jobs/JobsUiState.kt
@@ -1,8 +1,12 @@
 package com.cupcake.jobsfinder.ui.jobs
 
+import com.cupcake.jobsfinder.domain.model.JobWithTitle
+
 
 data class JobsUiState(
-    val job: List<JobUiState> = emptyList(),
+    val recommendedJobs: List<JobUiState> = emptyList(),
+    val topSalaryJobs: List<JobUiState> = emptyList(),
+    val inLocationJobs: List<JobUiState> = emptyList(),
     val isLoading: Boolean = false,
     val error: List<String> = emptyList()
 )
@@ -20,3 +24,12 @@ data class JobUiState(
     val location: String = "",
     val salary: String = ""
 )
+
+fun JobWithTitle.toJobUiState() = JobUiState(
+        image = "",
+        title = this.jobTitle.title,
+        companyName = this.company,
+        detailsChip = listOf(this.workType, this.jobType),
+        location = this.jobLocation,
+        salary = this.salary
+    )
