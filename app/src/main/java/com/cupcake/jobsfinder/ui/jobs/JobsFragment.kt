8fcd8454bc5e433d4ed09a858c0ac8215e75af3e
Hassan Ayman Ahmed <hassan3ayman@gmail.com>
2023-06-08 01:07:07 +0200

    setup jobsfragment and setup jobsadapter

--- app/src/main/java/com/cupcake/jobsfinder/ui/jobs/JobsFragment.kt
+++ app/src/main/java/com/cupcake/jobsfinder/ui/jobs/JobsFragment.kt
@@ -1,8 +1,16 @@
 package com.cupcake.jobsfinder.ui.jobs
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.viewModelScope
 import com.cupcake.jobsfinder.R
 import com.cupcake.jobsfinder.databinding.FragmentJobsBinding
 import com.cupcake.jobsfinder.ui.base.BaseFragment
+import com.cupcake.jobsfinder.ui.jobs.adapter.JobsAdapter
+import dagger.hilt.android.scopes.ViewModelScoped
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 
 class JobsFragment : BaseFragment<FragmentJobsBinding, JobsViewModel>(
@@ -11,5 +19,51 @@ class JobsFragment : BaseFragment<FragmentJobsBinding, JobsViewModel>(
 ) {
 
     override val LOG_TAG: String = this::class.java.name
+    private lateinit var jobsAdapter: JobsAdapter
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setUpAdapter()
+    }
 
+    private fun setUpAdapter() {
+        jobsAdapter = JobsAdapter(initialJobsItem(), viewModel)
+        loadJobsToAdapter()
+        binding?.jobsRecycler?.adapter = jobsAdapter
+    }
+
+    private fun loadJobsToAdapter() {
+        var jobs = mutableListOf<JobsItem>()
+        GlobalScope.launch {
+            viewModel.jobsUIState.collect {
+                val recommendedJobs = JobsItem.Recommended(it.recommendedJobs)
+                val topSalaryJobs = JobsItem.TopSalary(it.topSalaryJobs)
+
+                jobs = mutableListOf(recommendedJobs, topSalaryJobs, JobsItem.LocationJobs(JobUiState()))
+                jobs.addAll(it.inLocationJobs.map { jobUiState -> JobsItem.LocationJobs(jobUiState) })
+                Log.v("hassan", jobs.toString())
+            }
+        }
+    }
+
+    private fun initialJobsItem(): List<JobsItem> = listOf(
+        JobsItem.Recommended(listOf(
+            JobUiState("",
+                "Android",
+                companyName = "the chance",
+                detailsChip = listOf("full-time", "on site"),
+                location = "syria",
+                salary = "1000"))),
+        JobsItem.TopSalary(listOf(
+            JobUiState("",
+                "Android",
+                companyName = "the chance",
+                detailsChip = listOf("full-time", "on site"),
+                location = "syria",
+                salary = "1000"))),
+        JobsItem.LocationJobs( JobUiState("",
+            "Android",
+            companyName = "the chance",
+            detailsChip = listOf("full-time", "on site"),
+            location = "syria",
+            salary = "1000"))
+    )
 }
