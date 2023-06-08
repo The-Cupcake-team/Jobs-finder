533100a8add9bbfad070d3b4e782186910a721ab
ali mohamed ali <aliabuhussien371@gmail.com>
2023-06-08 20:43:12 +0300

    add JobDetailUiState and JobsDetailsUiState and change in JobViewModel

--- app/src/main/java/com/cupcake/jobsfinder/ui/jobs/JobViewModel.kt
+++ app/src/main/java/com/cupcake/jobsfinder/ui/jobs/JobViewModel.kt
@@ -1,8 +1,9 @@
 package com.cupcake.jobsfinder.ui.jobs
 
 
+
 import androidx.lifecycle.viewModelScope
-import com.cupcake.jobsfinder.domain.usecase.GetJobByIdUseCase
+import com.cupcake.jobsfinder.domain.useCase.GetJobByIdUseCase
 import com.cupcake.jobsfinder.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -15,30 +16,32 @@ class JobViewModel @Inject constructor(
     private val getJobByIdUseCase: GetJobByIdUseCase
 ) : BaseViewModel() {
 
-    private val _jobsUIState = MutableStateFlow(JobsUiState())
-    val jobsUIState: StateFlow<JobsUiState> = _jobsUIState
+    private val _jobsUIState = MutableStateFlow(JobDetailUiState(job =JobsDetailsUiState() ))
+    val jobsUIState: StateFlow<JobDetailUiState> = _jobsUIState
 
     private val errors: MutableList<ErrorUiState> = mutableListOf()
 
     init {
-        getJobById(1)
+        getJobById("438cd8f7-af62-4796-84be-a98807e874d8")
     }
 
-    private fun getJobById(jobId: Int) {
+    private fun getJobById(jobId: String) {
         _jobsUIState.value = _jobsUIState.value.copy(isLoading = true)
 
         viewModelScope.launch {
             try {
                 val job = getJobByIdUseCase(jobId)
-                val jobUiState = JobUiState(
+                val jobUiState = JobsDetailsUiState(
                     image = "",
                     title = job.jobTitleId.toString(),
                     companyName = job.company ?: "",
-                    detailsChip = listOf(job.workType.toString(), job.jobType.toString()),
+                    workType = job.workType.toString(),
+                    jobType=job.jobType.toString(),
                     location = job.jobLocation ?: "",
-                    salary = ""
+                    salary = "",
+                    createdAt = job.createdAt ?: 0,
                 )
-                _jobsUIState.value = _jobsUIState.value.copy(job = listOf(jobUiState), isLoading = false)
+                _jobsUIState.value = _jobsUIState.value.copy(job = jobUiState, isLoading = false)
             } catch (throwable: Throwable) {
                 errors.add(ErrorUiState(0, throwable.message.toString()))
                 _jobsUIState.value = _jobsUIState.value.copy(
--- app/src/main/java/com/cupcake/jobsfinder/ui/jobs/JobsUiState.kt
+++ app/src/main/java/com/cupcake/jobsfinder/ui/jobs/JobsUiState.kt
@@ -18,5 +18,21 @@ data class JobUiState(
     val companyName: String = "",
     val detailsChip: List<String> = emptyList(),
     val location: String = "",
-    val salary: String = ""
+    val salary: String = "",
+    val createdAt:Long=0,
+)
+data class JobDetailUiState(
+    val job: JobsDetailsUiState ,
+    val isLoading: Boolean = false,
+    val error: List<String> = emptyList()
+)
+data class JobsDetailsUiState(
+    val image: String = "",
+    val title: String = "",
+    val companyName: String = "",
+    val workType:String="",
+    val jobType:String = "",
+    val location: String = "",
+    val salary: String = "",
+    val createdAt:Long=0,
 )
