88ea329b7265d079bd3516b21b0d274f70941c33
Hasa9nAli <hasan.20cs75@student.uomosul.edu.iq>
2023-06-08 17:19:22 +0300

    installation the JobPagerAdapter on JobDetails

--- app/src/main/java/com/cupcake/jobsfinder/ui/jobs/JobDetails.kt
+++ app/src/main/java/com/cupcake/jobsfinder/ui/jobs/JobDetails.kt
@@ -1,5 +1,6 @@
 package com.cupcake.jobsfinder.ui.jobs
 
+import android.os.Bundle
 import com.cupcake.jobsfinder.R
 import com.cupcake.jobsfinder.databinding.FragmentJobDetailsBinding
 import com.cupcake.jobsfinder.ui.base.BaseFragment
@@ -9,6 +10,19 @@ class JobDetails : BaseFragment<FragmentJobDetailsBinding, JobViewModel>(
     R.layout.fragment_job_details,
     JobViewModel::class.java
 ) {
-    override val LOG_TAG: String =this.javaClass.simpleName
+    override val LOG_TAG: String = this.javaClass.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installJobViewPagerAdapter()
+    }
+
+    private fun installJobViewPagerAdapter() {
+        val aboutJobCategoryFragment = AboutJobCategory()
+        val aboutCompanyFragment = FragmentAboutCompanyCategory()
+        val listOfFragment = listOf(aboutJobCategoryFragment, aboutCompanyFragment)
+        val jobViewPager = JobPagerAdapter(this, listOfFragment)
+        binding?.viewPagerCategory?.adapter = jobViewPager
+    }
 
 }
