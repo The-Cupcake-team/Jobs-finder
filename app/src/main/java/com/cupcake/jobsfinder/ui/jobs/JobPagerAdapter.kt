dfa8bba803dd330dad92612dbbd280f5ac50eb53
Hasa9nAli <hasan.20cs75@student.uomosul.edu.iq>
2023-06-08 15:30:17 +0300

    create JobPagerAdapter

---
+++ app/src/main/java/com/cupcake/jobsfinder/ui/jobs/JobPagerAdapter.kt
@@ -0,0 +1,12 @@
+package com.cupcake.jobsfinder.ui.jobs
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class JobPagerAdapter(container: FragmentActivity, private val fragmentList: List<Fragment>) :
+    FragmentStateAdapter(container) {
+    override fun getItemCount() = fragmentList.size
+
+    override fun createFragment(position: Int) = fragmentList[position]
+}
