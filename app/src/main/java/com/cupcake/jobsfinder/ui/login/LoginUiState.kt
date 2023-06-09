e114728c41a510c9b55f751a8584c8404ba36eb5
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-06-09 23:18:49 +0300

    add login ui state

---
+++ app/src/main/java/com/cupcake/jobsfinder/ui/login/LoginUiState.kt
@@ -0,0 +1,15 @@
+package com.cupcake.jobsfinder.ui.login
+
+import java.lang.Error
+
+data class LoginUiState(
+    val userName: String = "",
+    val password: String = "",
+    val userNameError: String = "",
+    val passwordError: String = "",
+    val isUserNameValid: Boolean = false,
+    val isPasswordValid: Boolean = false,
+    val isLoading: Boolean = false,
+    val isValidFields: Boolean = false,
+    val error: String = ""
+    )
