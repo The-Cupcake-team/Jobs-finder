86d1c87d96673af383c0fd8ed6d3fccddaa9586d
Hasa9nAli <hasan.20cs75@student.uomosul.edu.iq>
2023-06-13 18:27:13 +0300

    add extension function for convert job to JobsDetailsUiState

--- presentation/viewmodels/src/main/java/com/cupcake/viewmodels/job_details/JobViewModel.kt
+++ presentation/viewmodels/src/main/java/com/cupcake/viewmodels/job_details/JobViewModel.kt
@@ -2,18 +2,23 @@ package com.cupcake.viewmodels.job_details
 
 
 import androidx.lifecycle.viewModelScope
+import com.cupcake.models.Job
+import com.cupcake.usecase.GetJobByIdUseCase
 import com.cupcake.viewmodels.base.BaseViewModel
 import com.cupcake.viewmodels.jobs.ErrorUiState
 import com.cupcake.viewmodels.jobs.JobsUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class JobViewModel @Inject constructor(
-//    private val getJobByIdUseCase: GetJobByIdUseCase
+    private val getJobByIdUseCase: GetJobByIdUseCase
 ) : BaseViewModel<JobsUiState>(JobsUiState()) {
 
     private val _jobsUIState = MutableStateFlow(
@@ -23,6 +28,22 @@ class JobViewModel @Inject constructor(
     )
     val jobsUIState: StateFlow<JobDetailUiState> = _jobsUIState
     private val errors: MutableList<ErrorUiState> = mutableListOf()
+
+
+    private fun Job.toJobsDetailsUiState(): JobsDetailsUiState {
+        return JobsDetailsUiState(
+            image = "",
+            title = "title",
+            companyName = this.company,
+            workType = this.workType,
+            jobType = this.jobType,
+            location = this.jobLocation,
+            salary = this.jobSalary.toString(),
+            createdAt = this.createdAt,
+            jobDescription = this.jobDescription
+        )
+    }
+
 
     init {
 //        getJobById("438cd8f7-af62-4796-84be-a98807e874d8")
