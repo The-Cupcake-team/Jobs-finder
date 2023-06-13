b1afa6ced11bb5f686d2381b9ec0ad27799b2910
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-06-13 19:24:04 +0300

    add update state to view model

--- presentation/viewmodels/src/main/java/com/cupcake/viewmodels/base/BaseViewModel.kt
+++ presentation/viewmodels/src/main/java/com/cupcake/viewmodels/base/BaseViewModel.kt
@@ -7,6 +7,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.lang.Exception
 
@@ -15,6 +16,9 @@ abstract class BaseViewModel<STATE>(initialUiState: STATE) : ViewModel() {
 
     protected val _state: MutableStateFlow<STATE> by lazy { MutableStateFlow(initialUiState) }
     val state: StateFlow<STATE> by lazy { _state.asStateFlow() }
+    protected fun updateState(block: (STATE) -> STATE) {
+        _state.update { block(it) }
+    }
 
     fun <T> tryToExecute(
         callee: suspend () -> T,
