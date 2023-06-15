f8a6f547c91a4f7e168955663ec7d2b435459c58
TarekIdrees <tarekdrias321@gmail.com>
2023-06-15 15:15:05 +0300

    handel header button click

--- presentation/viewmodels/src/main/java/com/cupcake/viewmodels/create_job/CreatJobEvent.kt
+++ presentation/viewmodels/src/main/java/com/cupcake/viewmodels/create_job/CreatJobEvent.kt
@@ -2,4 +2,5 @@ package com.cupcake.viewmodels.create_job
 
 sealed class CreateJobEvent {
     data class PageScrolled(val index: Int) : CreateJobEvent()
+    data class HeaderButtonClicked(val index: Int) : CreateJobEvent()
 }
---
+++ presentation/viewmodels/src/main/java/com/cupcake/viewmodels/create_job/CreateJobInteractionListener.kt
@@ -0,0 +1,5 @@
+package com.cupcake.viewmodels.create_job
+
+interface CreateJobInteractionListener {
+    fun onHeaderButtonClicked(id: Int)
+}
