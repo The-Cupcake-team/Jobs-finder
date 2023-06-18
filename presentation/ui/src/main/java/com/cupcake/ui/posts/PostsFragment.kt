94003bde1ab9b5390ff4a603e5ef7238305eee5d
David samuel <davidsamuelramsis@gmail.com>
2023-06-19 00:44:12 +0300

    add fun transformer view pager

--- presentation/ui/src/main/java/com/cupcake/ui/posts/PostsFragment.kt
+++ presentation/ui/src/main/java/com/cupcake/ui/posts/PostsFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
 import androidx.viewpager2.widget.ViewPager2
 import com.cupcake.ui.R
 import com.cupcake.ui.base.BaseFragment
@@ -14,6 +16,7 @@ import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlin.math.abs
 
 class PostsFragment : BaseFragment<FragmentPostsBinding, PostsViewModel>(
     R.layout.fragment_posts,
@@ -52,11 +55,32 @@ class PostsFragment : BaseFragment<FragmentPostsBinding, PostsViewModel>(
         }
     }
 
+    private fun setUpTransformerViewPager(viewPager: ViewPager2) {
+        val transformer = CompositePageTransformer()
+        transformer.addTransformer(MarginPageTransformer(20))
+        transformer.addTransformer { view, position ->
+            if (position <= -1.0F || position >= 1.0F) {
+                view.alpha = 0.2F
+                view.visibility = View.GONE
+            } else if (position == 0.0F) {
+                view.alpha = 1.0F
+                view.visibility = View.VISIBLE
+            } else {
+                view.alpha = 1.0F - abs(position)
+                view.visibility = View.VISIBLE
+            }
+        }
+        viewPager.setPageTransformer(transformer)
+    }
+
+
+
     private fun setTabLayout(tabLayoutCategory : TabLayout, viewPagerCategory : ViewPager2){
         val tabsName = listOf("Public", "Following")
         TabLayoutMediator(tabLayoutCategory, viewPagerCategory){ tab , position ->
             tab.text = tabsName[position]
         }.attach()
+        setUpTransformerViewPager(viewPagerCategory)
     }
 
 
