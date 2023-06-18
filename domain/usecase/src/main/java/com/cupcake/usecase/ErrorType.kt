0419fa20995ca283322a4dd1f63e92930b9e9967
Garfend <71102453+Garfend@users.noreply.github.com>
2023-06-19 02:36:38 +0300

    add new error types for register usecase

--- domain/usecase/src/main/java/com/cupcake/usecase/ErrorType.kt
+++ domain/usecase/src/main/java/com/cupcake/usecase/ErrorType.kt
@@ -3,4 +3,7 @@ package com.cupcake.usecase
 sealed class ErrorType(val messages: String) : Throwable() {
     class InvalidFieldPassword(message:String=""): ErrorType(message)
     class InvalidFieldUserName(message:String=""): ErrorType(message)
+    class InvalidFieldFullName(message:String=""): ErrorType(message)
+    class InvalidFieldConfirmedPassword(message:String=""): ErrorType(message)
+    class InvalidFieldEmail(message:String=""): ErrorType(message)
 }
