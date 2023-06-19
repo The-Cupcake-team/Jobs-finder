2701bc78a9c81049c93cf863a080060040b785bd
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-06-19 21:53:05 +0200

    update FollowingFragment

--- presentation/ui/src/main/java/com/cupcake/ui/posts/FollowingFragment.kt
+++ presentation/ui/src/main/java/com/cupcake/ui/posts/FollowingFragment.kt
@@ -1,25 +1,67 @@
 package com.cupcake.ui.posts
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import com.cupcake.ui.R
 import com.cupcake.ui.base.BaseFragment
 import com.cupcake.ui.databinding.FragmentFollowingBinding
 import com.cupcake.viewmodels.posts.FollowingPostsViewModel
+import com.cupcake.viewmodels.posts.PostsEvent
+import kotlinx.coroutines.launch
 
 class FollowingFragment : BaseFragment<FragmentFollowingBinding, FollowingPostsViewModel>(
     R.layout.fragment_following,
     FollowingPostsViewModel::class.java
 ) {
     override val LOG_TAG: String = this.javaClass.simpleName
+    private val bottomSheetFragment = BottomSheetFragment()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         setupPostsRecyclerView()
+        handleEvent()
     }
 
     private fun setupPostsRecyclerView(){
         val adapter = PostsAdapter(listOf(), viewModel)
         binding.recyclerViewFollowingPosts.adapter = adapter
+        binding.recyclerViewFollowingPosts.itemAnimator = null
+
+    }
+
+    private fun handleEvent(){
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.postEvent.collect{postEvent ->
+                    postEvent.getContentIfNotHandled()?.let { event ->
+                        when(event){
+                            is PostsEvent.PostCommentClick -> {
+                                val action = PostsFragmentDirections.actionPostsFragmentToCommentsFragment("27271a1d-3023-44e1-bfb4-130afa641438")
+                                findNavController().navigate(action)
+                            }
+
+                            is PostsEvent.PostShareClick -> {
+                                val shareIntent= Intent().apply {
+                                    action = Intent.ACTION_SEND
+                                    putExtra(Intent.EXTRA_TEXT, "This is my text to send.")
+                                    type = "text/plain"
+                                }
+                                startActivity(Intent.createChooser(shareIntent, "Share via"))
+
+                            }
+                            is PostsEvent.PostOptionsClick -> {
+                                bottomSheetFragment.show(requireActivity().supportFragmentManager, "BottomSheetDialog")
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
     }
 
 }
