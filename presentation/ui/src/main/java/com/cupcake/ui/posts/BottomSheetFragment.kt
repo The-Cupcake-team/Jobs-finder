c97fc3b63558fa0a817d2c90ba7b2b56ef13b8e4
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-06-19 21:50:51 +0200

    add bottom sheet

---
+++ presentation/ui/src/main/java/com/cupcake/ui/posts/BottomSheetFragment.kt
@@ -0,0 +1,28 @@
+package com.cupcake.ui.posts
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.cupcake.ui.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class BottomSheetFragment : BottomSheetDialogFragment() {
+
+    private lateinit var binding: ViewDataBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.post_bottom_sheet, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
