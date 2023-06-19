fab96fbea12b90daba5a750c8395b9c64f4aab36
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-06-19 21:56:59 +0200

    update post interface

--- presentation/viewmodels/src/main/java/com/cupcake/viewmodels/posts/PostInteractionListener.kt
+++ presentation/viewmodels/src/main/java/com/cupcake/viewmodels/posts/PostInteractionListener.kt
@@ -5,4 +5,8 @@ import com.cupcake.viewmodels.base.BaseInteractionListener
 interface PostInteractionListener: BaseInteractionListener {
 
     fun onCommentClick(id: String)
+    fun onLikeClick(id: String)
+    fun onClickShare(id: String)
+    fun onOptionsClick()
+
 }
