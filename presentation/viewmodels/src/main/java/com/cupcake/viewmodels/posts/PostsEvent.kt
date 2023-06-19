5dd8e30e7194e2bb40bcab025f146b0351c7f77f
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-06-19 21:57:21 +0200

    update posts event

--- presentation/viewmodels/src/main/java/com/cupcake/viewmodels/posts/PostsEvent.kt
+++ presentation/viewmodels/src/main/java/com/cupcake/viewmodels/posts/PostsEvent.kt
@@ -2,4 +2,6 @@ package com.cupcake.viewmodels.posts
 
 sealed class PostsEvent{
     data class PostCommentClick(val id: String): PostsEvent()
+    data class PostShareClick(val id: String): PostsEvent()
+   object PostOptionsClick: PostsEvent()
 }
