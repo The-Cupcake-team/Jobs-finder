1d517e85eef3389b28ec63210c3f895272dc41bc
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2023-06-19 12:08:40 +0300

    add on Conflict Strategy to insert job funciton

--- data/local/src/main/java/com/cupcake/jobsfinder/local/daos/JobFinderDao.kt
+++ data/local/src/main/java/com/cupcake/jobsfinder/local/daos/JobFinderDao.kt
@@ -2,10 +2,11 @@ package com.cupcake.jobsfinder.local.daos
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import com.cupcake.jobsfinder.local.entities.JobsEntity
 
 @Dao
 interface JobFinderDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertJop(job: JobsEntity)
 }
