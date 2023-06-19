98cd1f68a1026824ce31dfb376b0af711e4e16ba
TarekIdrees <tarekdrias321@gmail.com>
2023-06-19 18:18:31 +0300

    use tryToExecute to remove redundant code

--- presentation/viewmodels/src/main/java/com/cupcake/viewmodels/job_details/JobViewModel.kt
+++ presentation/viewmodels/src/main/java/com/cupcake/viewmodels/job_details/JobViewModel.kt
@@ -1,52 +1,40 @@
 package com.cupcake.viewmodels.job_details
 
-
-import androidx.lifecycle.viewModelScope
 import com.cupcake.models.Job
 import com.cupcake.usecase.GetJobByIdUseCase
+import com.cupcake.viewmodels.base.BaseErrorUiState
 import com.cupcake.viewmodels.base.BaseViewModel
-import com.cupcake.viewmodels.jobs.ErrorUiState
-import com.cupcake.viewmodels.jobs.JobsUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class JobViewModel @Inject constructor(
     private val getJobByIdUseCase: GetJobByIdUseCase
-) : BaseViewModel<JobsUiState>(JobsUiState()) {
+) : BaseViewModel<JobDetailUiState>(JobDetailUiState()) {
 
-    private val _jobsUIState = MutableStateFlow(
-        JobDetailUiState(job = JobsDetailsUiState())
-    )
-    val jobsUIState: StateFlow<JobDetailUiState> = _jobsUIState
-    private val errors: MutableList<ErrorUiState> = mutableListOf()
-    private val handler = CoroutineExceptionHandler { _, exception ->
-        onGetJobDetailsFailure(exception)
-    }
-
-    private fun onGetJobDetailsFailure(exception: Throwable) {
-        this._jobsUIState.update {
-            it.copy(
-                isLoading = false,
-                error = listOf(exception.message.toString())
-            )
-        }
+    init {
+        getJobDetails("438cd8f7-af62-4796-84be-a98807e874d8")
     }
 
     private fun getJobDetails(jobId: String) {
-        viewModelScope.launch(Dispatchers.IO + handler) {
-            val jobDetails = getJobByIdUseCase(jobId)
-            _jobsUIState.value = _jobsUIState.value.copy(
+        tryToExecute(
+            { getJobByIdUseCase(jobId) },
+            ::onGetJobDetailsSuccess,
+            ::onGetJobDetailsFailure
+        )
+    }
+
+    private fun onGetJobDetailsSuccess(jobDetails: Job) {
+        updateState {
+            it.copy(
                 isLoading = false,
+                error = null,
                 job = jobDetails.toJobsDetailsUiState()
-            )
-        }
+            ) }
+    }
+
+    private fun onGetJobDetailsFailure(error: BaseErrorUiState) {
+        updateState { it.copy(isLoading = false, error = error) }
     }
 
     private fun Job.toJobsDetailsUiState(): JobsDetailsUiState {
@@ -57,13 +45,9 @@ class JobViewModel @Inject constructor(
             workType = workType,
             jobType = jobType,
             location = jobLocation,
-            salary = jobSalary.toString(),
+            salary = jobSalary.toInt(),
             createdAt = createdAt,
             jobDescription = jobDescription
         )
-    }
-
-    init {
-        getJobDetails("438cd8f7-af62-4796-84be-a98807e874d8")
     }
 }
--- presentation/viewmodels/src/main/java/com/cupcake/viewmodels/job_details/JobsUiState.kt
+++ presentation/viewmodels/src/main/java/com/cupcake/viewmodels/job_details/JobsUiState.kt
@@ -1,6 +1,6 @@
 package com.cupcake.viewmodels.job_details
 
-import com.cupcake.models.JobWithTitle
+import com.cupcake.viewmodels.base.BaseErrorUiState
 
 data class JobDetailsUiState(
     val image: String = "",
@@ -12,9 +12,9 @@ data class JobDetailsUiState(
     val createdAt:Long=0,
 )
 data class JobDetailUiState(
-    val job: JobsDetailsUiState,
-    val isLoading: Boolean = false,
-    val error: List<String> = emptyList()
+    val job: JobsDetailsUiState = JobsDetailsUiState(),
+    val isLoading: Boolean = true,
+    val error: BaseErrorUiState? = null
 )
 data class JobsDetailsUiState(
     val image: String = "",
@@ -23,18 +23,8 @@ data class JobsDetailsUiState(
     val workType:String="",
     val jobType:String = "",
     val location: String = "",
-    val salary: String = "",
+    val salary: Int = 0,
     val createdAt:Long=0,
     val jobDescription:String=""
 )
-
-
-fun JobWithTitle.toJobUiState() = JobDetailsUiState(
-        image = "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://coursera-course-photos.s3.amazonaws.com/e3/f27630d13511e88dd241e68ded0cea/K_logo_800x800.png?auto=format%2Ccompress&dpr=1",
-        title = this.jobTitle.title,
-        companyName = this.company,
-        detailsChip = listOf(this.workType, this.jobType),
-        location = this.jobLocation,
-        salary = this.salary
-    )
 
