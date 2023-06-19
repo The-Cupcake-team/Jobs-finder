4ee0734fc6cf7c9f76e7f5d8a63053e6e7547347
Abdelrahman Sedky <46361405+Abdelrahman-Sedky@users.noreply.github.com>
2023-06-20 00:15:09 +0300

    update register ui state

--- presentation/viewmodels/src/main/java/com/cupcake/viewmodels/register/RegisterUiState.kt
+++ presentation/viewmodels/src/main/java/com/cupcake/viewmodels/register/RegisterUiState.kt
@@ -3,31 +3,12 @@ package com.cupcake.viewmodels.register
 import com.cupcake.viewmodels.base.BaseErrorUiState
 
 data class RegisterUiState(
-    var fullName: String = "",
-    var userName: String = "",
-    var email: String = "",
-    var password: String = "",
-    var confirmedPassword: String = "",
-
-
-    val isFullNameValid: Boolean = false,
-    val isUserNameValid: Boolean = false,
-    val isEmailValid: Boolean = false,
-    val isPasswordValid: Boolean = false,
-    val isConfirmedPasswordValid: Boolean = false,
-
-
-    val fullNameError: String = "",
-    val userNameError: String = "",
-    val emailError: String = "",
-    val passwordError: String = "",
-    val confirmedPasswordError: String = "",
-
-    val fieldName: String = "",
-    val fieldError: String = "",
-    val isFieldValid: Boolean = false,
-
-
+    val fullName: String = "",
+    val userName: String = "",
+    val email: String = "",
+    val password: String = "",
+    val confirmPassword: String = "",
+    val isUserRegistered: Boolean = false,
     val isLoading: Boolean = false,
     val error: BaseErrorUiState? = null,
 )
