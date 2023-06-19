e9c17437995d4a13a663477cbd0cd9d6e672fa50
Abdelrahman Sedky <46361405+Abdelrahman-Sedky@users.noreply.github.com>
2023-06-20 00:15:26 +0300

    update register fragment

--- presentation/ui/src/main/java/com/cupcake/ui/register/RegisterFragment.kt
+++ presentation/ui/src/main/java/com/cupcake/ui/register/RegisterFragment.kt
@@ -1,23 +1,46 @@
 package com.cupcake.ui.register
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.cupcake.ui.R
 import com.cupcake.ui.base.BaseFragment
-import com.cupcake.ui.databinding.FragmentLoginBinding
 import com.cupcake.ui.databinding.FragmentRegisterBinding
-import com.cupcake.viewmodels.login.LoginViewModel
+import com.cupcake.viewmodels.register.RegisterEvent
 import com.cupcake.viewmodels.register.RegisterViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class RegisterFragment: BaseFragment<FragmentRegisterBinding ,  RegisterViewModel>(
-
+class RegisterFragment : BaseFragment<FragmentRegisterBinding, RegisterViewModel>(
     R.layout.fragment_register,
     RegisterViewModel::class.java
-    ) {
-        override val LOG_TAG :String= this.javaClass.name
+) {
+    override val LOG_TAG: String = this.javaClass.name
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        handleRegisterEvents()
     }
 
+    private fun handleRegisterEvents() {
+        lifecycleScope.launch(Dispatchers.Main) {
+            viewModel.event.collect {
+                when (it) {
+                    is RegisterEvent.LoginClick -> navigateToLogin()
+                    is RegisterEvent.ShowErrorMessage -> showToast(it.errorMessage)
+
+                }
+            }
+        }
+    }
+
+    private fun navigateToLogin() {
+        findNavController().navigate(RegisterFragmentDirections.actionRegisterFragmentToLoginFragment())
+    }
+
+    private fun showToast(errorMessage: String) {
+        Toast.makeText(requireContext(), errorMessage, Toast.LENGTH_SHORT).show()
+    }
+}
