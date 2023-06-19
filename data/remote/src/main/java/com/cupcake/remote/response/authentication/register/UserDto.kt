9208094c7c5a0bb0191240cf55bdb8a7260b752f
Abdelrahman Sedky <46361405+Abdelrahman-Sedky@users.noreply.github.com>
2023-06-20 00:13:18 +0300

    add UserDto

---
+++ data/remote/src/main/java/com/cupcake/remote/response/authentication/register/UserDto.kt
@@ -0,0 +1,14 @@
+package com.cupcake.remote.response.authentication.register
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UserDto(
+    val id : String,
+    val username : String,
+    val fullName : String,
+    val phoneNumber : String,
+    val isActive : Boolean,
+    val createdAt : Long,
+    val token : Token
+)
