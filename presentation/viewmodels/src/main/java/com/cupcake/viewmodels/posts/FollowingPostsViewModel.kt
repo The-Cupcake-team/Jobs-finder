33321916e6c8627b8e24f87c21217c98cd5505af
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-06-19 21:53:22 +0200

    update FollowingPostsViewModel

--- presentation/viewmodels/src/main/java/com/cupcake/viewmodels/posts/FollowingPostsViewModel.kt
+++ presentation/viewmodels/src/main/java/com/cupcake/viewmodels/posts/FollowingPostsViewModel.kt
@@ -5,6 +5,7 @@ import com.cupcake.models.Post
 import com.cupcake.usecase.GetFollowingPostsUseCase
 import com.cupcake.viewmodels.base.BaseErrorUiState
 import com.cupcake.viewmodels.base.BaseViewModel
+import com.cupcake.viewmodels.utill.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
@@ -17,7 +18,7 @@ class FollowingPostsViewModel @Inject constructor(
     private val getFollowingPostsUseCase: GetFollowingPostsUseCase
 ) : BaseViewModel<PostsUIState>(PostsUIState()), PostInteractionListener {
 
-    private val _postEvent = MutableSharedFlow<PostsEvent>()
+    private val _postEvent = MutableSharedFlow<Event<PostsEvent>>()
     val postEvent = _postEvent.asSharedFlow()
 
     init {
@@ -47,6 +48,11 @@ class FollowingPostsViewModel @Inject constructor(
             it.copy(isLoading = false, isError = true, error = error)
         }
     }
+    fun onRetryClicked(){
+        _state.update {it.copy(error = null, isLoading = true) }
+        getFollowingPosts()
+    }
+
 
     private fun Post.toPostItemUIState(): PostItemUIState {
         return PostItemUIState(
@@ -58,8 +64,37 @@ class FollowingPostsViewModel @Inject constructor(
 
     override fun onCommentClick(id: String) {
         viewModelScope.launch {
-            _postEvent.emit(PostsEvent.PostCommentClick(id))
+            _postEvent.emit(Event(PostsEvent.PostCommentClick(id)))
         }
     }
+
+    override fun onLikeClick(id: String) {
+        _state.update { currentState ->
+            val updatedPosts = currentState.postsResult.map { post ->
+                if (post.id == id) {
+                    val newLikes = if (post.isLiked) post.likes - 1 else post.likes + 1
+                    post.copy(likes = newLikes, isLiked = !post.isLiked)
+                } else {
+                    post
+                }
+            }
+            currentState.copy(postsResult = updatedPosts)
+        }
+    }
+
+    override fun onClickShare(id: String) {
+        viewModelScope.launch {
+            _postEvent.emit(Event(PostsEvent.PostShareClick(id)))
+        }
+    }
+
+    override fun onOptionsClick() {
+        viewModelScope.launch {
+            _postEvent.emit(Event(PostsEvent.PostOptionsClick))
+        }
+    }
+
+
+
 
 }
