2b20031e5841a8ca367173077e3f27c1ad332906
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-06-20 23:15:17 +0200

    add remove from back stack

--- presentation/ui/src/main/java/com/cupcake/ui/comment/CommentsFragment.kt
+++ presentation/ui/src/main/java/com/cupcake/ui/comment/CommentsFragment.kt
@@ -2,6 +2,8 @@ package com.cupcake.ui.comment
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.cupcake.ui.R
 import com.cupcake.ui.base.BaseFragment
 import com.cupcake.ui.databinding.FragmentCommentsBinding
@@ -19,6 +21,8 @@ class CommentsFragment : BaseFragment<FragmentCommentsBinding, CommentViewModel>
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         setupCommentRecyclerView()
         viewModel.getPost(args.postID)
+        requireActivity().actionBar?.title = ""
+        NavigationUI.setupWithNavController(binding.toolBar, findNavController())
     }
 
     private fun setupCommentRecyclerView(){
