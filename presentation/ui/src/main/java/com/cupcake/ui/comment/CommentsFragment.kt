4b1e611c6875e93483f396e7e50744c25f135723
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-06-20 22:51:59 +0200

    update comment fragment

--- presentation/ui/src/main/java/com/cupcake/ui/comment/CommentsFragment.kt
+++ presentation/ui/src/main/java/com/cupcake/ui/comment/CommentsFragment.kt
@@ -13,12 +13,17 @@ class CommentsFragment : BaseFragment<FragmentCommentsBinding, CommentViewModel>
     CommentViewModel::class.java
 ) {
     override val LOG_TAG: String = this.javaClass.name
+    private val args: CommentsFragmentArgs by lazy {
+        CommentsFragmentArgs.fromBundle(requireArguments())
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         setupCommentRecyclerView()
+        viewModel.getPost(args.postID)
     }
 
     private fun setupCommentRecyclerView(){
         val adapter = CommentAdapter(listOf(), viewModel)
         binding.RecyclerViewComments.adapter = adapter
+        binding.RecyclerViewComments.itemAnimator = null
     }
 }
