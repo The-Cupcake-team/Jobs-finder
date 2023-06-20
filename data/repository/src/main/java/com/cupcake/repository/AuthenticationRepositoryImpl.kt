c26ff4f29c71fa56ecf227edab5d58399c1a771f
Abdelrahman Sedky <46361405+Abdelrahman-Sedky@users.noreply.github.com>
2023-06-20 21:00:47 +0300

    delete unnecessary logs

--- data/repository/src/main/java/com/cupcake/repository/AuthenticationRepositoryImpl.kt
+++ data/repository/src/main/java/com/cupcake/repository/AuthenticationRepositoryImpl.kt
@@ -1,6 +1,5 @@
 package com.cupcake.repository
 
-import android.util.Log
 import com.cupcake.models.ErrorType
 import com.cupcake.models.User
 import com.cupcake.remote.JobApiService
@@ -51,17 +50,13 @@ class AuthenticationRepositoryImpl @Inject constructor(
         if (response.isSuccessful) {
             val baseResponse = response.body()
             if (baseResponse != null && baseResponse.isSuccess) {
-                Log.i("TAG", "base successful : ${baseResponse.value}")
                 return baseResponse.value!!
             } else {
-                Log.i("TAG", "base error : ${baseResponse?.message!!}")
-                throw ErrorType.Server(baseResponse.message)
+                throw ErrorType.Server(baseResponse?.message!!)
             }
         } else {
             val errorResponse = response.errorBody()?.toString()
-            Log.i("TAG", "base error : ${errorResponse ?: "Error Network"}")
             throw ErrorType.Server(errorResponse ?: "Error Network")
         }
-
     }
 }
