d2f6a467657558921c469211e51060325d8d21a4
David samuel <davidsamuelramsis@gmail.com>
2023-06-20 14:01:34 +0300

    fix loading bug and add set setupBottomSheetButtons

--- presentation/ui/src/main/java/com/cupcake/ui/posts/BottomSheetFragment.kt
+++ presentation/ui/src/main/java/com/cupcake/ui/posts/BottomSheetFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import com.cupcake.ui.R
@@ -17,12 +19,25 @@ class BottomSheetFragment : BottomSheetDialogFragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.post_bottom_sheet, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupBottomSheetButtons()
+    }
+
+    private fun setupBottomSheetButtons(){
+        val followButton = binding.root.findViewById<Button>(R.id.followTextView)
+        followButton.setOnClickListener {
+            Toast.makeText(requireContext(), "Follow button clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        val saveButton = binding.root.findViewById<Button>(R.id.saveTextView)
+        saveButton.setOnClickListener {
+            Toast.makeText(requireContext(), "Save button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
