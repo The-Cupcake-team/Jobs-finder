19d83a7f102dd4f8a385f5efc88e50612d460bec
Garfend <71102453+Garfend@users.noreply.github.com>
2023-06-22 01:23:42 +0300

    add error and valid ui states for each input

--- presentation/viewmodels/src/main/java/com/cupcake/viewmodels/register/RegisterUiState.kt
+++ presentation/viewmodels/src/main/java/com/cupcake/viewmodels/register/RegisterUiState.kt
@@ -3,12 +3,34 @@ package com.cupcake.viewmodels.register
 import com.cupcake.viewmodels.base.BaseErrorUiState
 
 data class RegisterUiState(
-    val fullName: String = "",
-    val userName: String = "",
-    val email: String = "",
-    val password: String = "",
-    val confirmPassword: String = "",
-    val isUserRegistered: Boolean = false,
+    var fullName: String = "",
+    var userName: String = "",
+    var email: String = "",
+    var password: String = "",
+    var confirmedPassword: String = "",
+
+
+    val isFullNameValid: Boolean = false,
+    val isUserNameValid: Boolean = false,
+    val isEmailValid: Boolean = false,
+    val isPasswordValid: Boolean = false,
+    val isConfirmedPasswordValid: Boolean = false,
+
+
+    val fullNameError: String = "",
+    val userNameError: String = "",
+    val emailError: String = "",
+    val passwordError: String = "",
+    val confirmedPasswordError: String = "",
+
+    val fieldName: String = "",
+    val fieldError: String = "",
+    val isFieldValid: Boolean = false,
+
+
     val isLoading: Boolean = false,
+
+    val isUserRegistered: Boolean = false,
+
     val error: BaseErrorUiState? = null,
 )
