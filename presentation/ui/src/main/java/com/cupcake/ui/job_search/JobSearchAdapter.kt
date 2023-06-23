aedf9c4447d33982b40a2ca65f1f4aac5d35ec96
HassanAyman <hassan3ayman@gmail.com>
2023-06-23 22:10:59 +0200

    make adapter for search result and complete search fragment

--- presentation/ui/src/main/java/com/cupcake/ui/job_search/JobSearchAdapter.kt
+++ presentation/ui/src/main/java/com/cupcake/ui/job_search/JobSearchAdapter.kt
@@ -2,10 +2,11 @@ package com.cupcake.ui.job_search
 
 import com.cupcake.ui.R
 import com.cupcake.ui.base.BaseAdapter
+import com.cupcake.viewmodels.job_search.JobsSearchInteractionListener
 import com.cupcake.viewmodels.jobs.JobUiState
 import com.cupcake.viewmodels.jobs.JobsListener
 
-class JobSearchAdapter(items: List<JobUiState>, listener: JobsListener) :
+class JobSearchAdapter(items: List<JobUiState>, listener: JobsSearchInteractionListener) :
     BaseAdapter<JobUiState>(items, listener) {
-    override var layoutId: Int = R.layout.item_job_card
+    override var layoutId: Int = R.layout.item_job_search_card
 }
--- presentation/ui/src/main/java/com/cupcake/ui/job_search/JobSearchFragment.kt
+++ presentation/ui/src/main/java/com/cupcake/ui/job_search/JobSearchFragment.kt
@@ -2,14 +2,21 @@ package com.cupcake.ui.job_search
 
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
+import com.cupcake.ui.BR
 import com.cupcake.ui.R
 import com.cupcake.ui.base.BaseFragment
 import com.cupcake.ui.databinding.BottomSheetJobsSearchFilterBinding
 import com.cupcake.ui.databinding.FragmentJobSearchBinding
+import com.cupcake.ui.jobs.adapter.JobsAdapter
 import com.cupcake.viewmodels.job_search.JobSearchViewModel
+import com.cupcake.viewmodels.job_search.SearchJobEvent
+import com.cupcake.viewmodels.jobs.JobsEvent
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class JobSearchFragment : BaseFragment<FragmentJobSearchBinding, JobSearchViewModel>(
@@ -21,19 +28,18 @@ class JobSearchFragment : BaseFragment<FragmentJobSearchBinding, JobSearchViewMo
 
     private val args: JobSearchFragmentArgs by navArgs()
 
+    private lateinit var dialog: BottomSheetDialog
+    private lateinit var bottomSheetBinding: BottomSheetJobsSearchFilterBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setUpAdapter()
         onBackButtonClicked()
+        setUpBottomSheet()
+        handelJobsSearchEvent()
         binding.editTextSearchInput.setText(args.jobTitle)
 
-    }
-
-    override fun onResume() {
-        super.onResume()
-        binding.imageViewFilter.setOnClickListener {
-            showFilterSheet()
-        }
     }
 
     private fun onBackButtonClicked(){
@@ -42,14 +48,28 @@ class JobSearchFragment : BaseFragment<FragmentJobSearchBinding, JobSearchViewMo
         }
     }
 
+    private fun setUpAdapter() {
+        val jobsAdapter = JobSearchAdapter(emptyList(), viewModel)
+        binding.recyclerJobSearch.adapter = jobsAdapter
+    }
 
-    private fun showFilterSheet() {
-        val bottomSheetBinding = BottomSheetJobsSearchFilterBinding.inflate(layoutInflater)
-        val dialog = BottomSheetDialog(requireContext())
+    private fun setUpBottomSheet(){
+        bottomSheetBinding = BottomSheetJobsSearchFilterBinding.inflate(layoutInflater)
+        dialog = BottomSheetDialog(requireContext())
+        dialog.setContentView(bottomSheetBinding.root)
+        bottomSheetBinding.setVariable(BR.viewModel, viewModel)
+    }
 
-        dialog.apply {
-            setContentView(bottomSheetBinding.root)
-            show()
+    private fun handelJobsSearchEvent() {
+         lifecycleScope.launch(Dispatchers.Main) {
+            viewModel.event.collect { jobsEvent ->
+                when (jobsEvent) {
+                    is SearchJobEvent.JobCardClick -> dialog.dismiss()
+                    is SearchJobEvent.OnApplyButtonClicked -> dialog.dismiss()
+                    is SearchJobEvent.OnFilterClicked -> dialog.show()
+                    is SearchJobEvent.OnMoreOptionClickListener -> dialog.dismiss()
+                }
+            }
         }
     }
 
