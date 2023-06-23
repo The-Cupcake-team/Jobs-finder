35c13145481d40f8ec429cfeced9090764a19530
HassanAyman <hassan3ayman@gmail.com>
2023-06-23 22:07:58 +0200

    add item search card and add listener to it

---
+++ presentation/viewmodels/src/main/java/com/cupcake/viewmodels/job_search/JobsSearchInteractionListener.kt
@@ -0,0 +1,10 @@
+package com.cupcake.viewmodels.job_search
+
+import com.cupcake.viewmodels.base.BaseInteractionListener
+
+interface JobsSearchInteractionListener : BaseInteractionListener {
+    fun onCardClickListener(id: String)
+
+    fun onImageViewMoreClickListener(model:JopSearchUIState)
+
+}
