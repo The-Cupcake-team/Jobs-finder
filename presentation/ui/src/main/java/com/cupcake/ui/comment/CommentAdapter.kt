bea025518b906f333ffd39cc33ba181e092d5c22
David samuel <davidsamuelramsis@gmail.com>
2023-06-24 14:54:39 +0300

    change item to comment ui state and  add get comment post in fragmrnt

--- presentation/ui/src/main/java/com/cupcake/ui/comment/CommentAdapter.kt
+++ presentation/ui/src/main/java/com/cupcake/ui/comment/CommentAdapter.kt
@@ -4,18 +4,17 @@ import com.cupcake.ui.R
 import com.cupcake.ui.base.BaseAdapter
 import com.cupcake.viewmodels.comment.CommentInteractionListener
 import com.cupcake.viewmodels.comment.CommentUiState
-import com.cupcake.viewmodels.posts.PostItemUIState
 
 class CommentAdapter(
-    items: List<CommentUiState.PostUiState>,
+    items: List<CommentUiState.CommentUiState>,
     listener: CommentInteractionListener,
 ) :
-    BaseAdapter<CommentUiState.PostUiState>(items, listener) {
+    BaseAdapter<CommentUiState.CommentUiState>(items, listener) {
     override var layoutId = R.layout.item_comment
 
     override fun areItemsEqual(
-        oldItem: CommentUiState.PostUiState,
-        newItem: CommentUiState.PostUiState,
+        oldItem: CommentUiState.CommentUiState,
+        newItem: CommentUiState.CommentUiState,
     ): Boolean {
         return oldItem.id == newItem.id
     }
