c084af23cdb99945300abec1c71f4da99f1dac06
ali mohamed ali <aliabuhussien371@gmail.com>
2023-06-24 17:29:47 +0300

    add fragment_profile_jobs.xml and item_profile_saved_jobs.xml

---
+++ presentation/ui/src/main/java/com/cupcake/ui/profile/resume/ProfileJobsFragment.kt
@@ -0,0 +1,19 @@
+package com.cupcake.ui.profile.resume
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.cupcake.ui.R
+import com.cupcake.ui.base.BaseFragment
+import com.cupcake.ui.databinding.FragmentProfilePostBinding
+import com.cupcake.viewmodels.posts.PostsViewModel
+
+
+class ProfileJobsFragment : BaseFragment<FragmentProfilePostBinding,PostsViewModel>(
+    R.layout.fragment_profile_post,
+    PostsViewModel::class.java
+) {
+    override val LOG_TAG: String = this::class.java.name
+}
