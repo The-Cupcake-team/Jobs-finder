04322d0c7e0289bb02d49b26f9f014349682c73d
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-06-26 02:57:32 +0300

    update adapter

--- presentation/ui/src/main/java/com/cupcake/ui/comment/CommentAdapter.kt
+++ presentation/ui/src/main/java/com/cupcake/ui/comment/CommentAdapter.kt
@@ -3,18 +3,18 @@ package com.cupcake.ui.comment
 import com.cupcake.ui.R
 import com.cupcake.ui.base.BaseAdapter
 import com.cupcake.viewmodels.comment.CommentInteractionListener
-import com.cupcake.viewmodels.comment.CommentUiState
+import com.cupcake.viewmodels.comment.CommentsUiState
 
 class CommentAdapter(
-    items: List<CommentUiState.CommentUiState>,
+    items: List<CommentsUiState.CommentUiState>,
     listener: CommentInteractionListener,
 ) :
-    BaseAdapter<CommentUiState.CommentUiState>(items, listener) {
+    BaseAdapter<CommentsUiState.CommentUiState>(items, listener) {
     override var layoutId = R.layout.item_comment
 
     override fun areItemsEqual(
-        oldItem: CommentUiState.CommentUiState,
-        newItem: CommentUiState.CommentUiState,
+        oldItem: CommentsUiState.CommentUiState,
+        newItem: CommentsUiState.CommentUiState,
     ): Boolean {
         return oldItem.id == newItem.id
     }
