79245863e3d905764fb27db9ca10f00dea423145
Garfend <71102453+Garfend@users.noreply.github.com>
2023-06-26 04:36:59 +0300

    create usecase for recent job and saved job

---
+++ domain/usecase/src/main/java/com/cupcake/usecase/profile_job/GetSavedJobsUseCas.kt
@@ -0,0 +1,64 @@
+package com.cupcake.usecase.profile_job
+
+import com.cupcake.models.Job
+import com.cupcake.models.JobSalary
+import com.cupcake.models.JobTitle
+import repo.JobFinderRepository
+import javax.inject.Inject
+
+class GetSavedJobsUseCas @Inject constructor(
+    private val repository: JobFinderRepository
+){
+        suspend operator fun invoke() : List<Job>{
+        return listOf(Job("12",
+            JobTitle(12),
+            "zrebafarm",
+            "2022",
+            "220",
+            "ezreba",
+            "fala7",
+            "zereba Education",
+            JobSalary(2.3,2.5),
+            "3.5"
+            ,"mm",
+            emptyList()
+        ),Job("12",
+            JobTitle(12),
+            "zrebafarm",
+            "2022",
+            "220",
+            "ezreba",
+            "fala7",
+            "zereba Education",
+            JobSalary(2.3,2.5),
+            "3.5"
+            ,"mm",
+            emptyList()
+        ),Job("12",
+            JobTitle(12),
+            "zrebafarm",
+            "2022",
+            "220",
+            "ezreba",
+            "fala7",
+            "zereba Education",
+            JobSalary(2.3,2.5),
+            "3.5"
+            ,"mm",
+            emptyList()
+        ),Job("12",
+            JobTitle(12),
+            "zrebafarm",
+            "2022",
+            "220",
+            "ezreba",
+            "fala7",
+            "zereba Education",
+            JobSalary(2.3,2.5),
+            "3.5"
+            ,"mm",
+            emptyList()
+        )
+        )
+    }
+}
