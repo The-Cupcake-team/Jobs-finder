ee7429c7a540b9fee677896e551b1d305399aabd
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-06-27 19:38:19 +0300

    update job event

--- presentation/viewmodels/src/main/java/com/cupcake/viewmodels/create_job/CreatJobEvent.kt
+++ presentation/viewmodels/src/main/java/com/cupcake/viewmodels/create_job/CreatJobEvent.kt
@@ -1,6 +1,5 @@
 package com.cupcake.viewmodels.create_job
 
 sealed class CreateJobEvent {
-    data class PageScrolled(val index: Int) : CreateJobEvent()
-    data class HeaderButtonClicked(val index: Int) : CreateJobEvent()
+    object NextPage: CreateJobEvent()
 }
