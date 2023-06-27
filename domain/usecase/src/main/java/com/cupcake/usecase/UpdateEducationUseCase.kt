63b049eddc55e845bca7f222e5ae1b0b110fdc83
HassanAyman <hassan3ayman@gmail.com>
2023-06-27 21:20:19 +0200

    valide the update education & make string values

--- domain/usecase/src/main/java/com/cupcake/usecase/UpdateEducationUseCase.kt
+++ domain/usecase/src/main/java/com/cupcake/usecase/UpdateEducationUseCase.kt
@@ -8,6 +8,23 @@ class UpdateEducationUseCase @Inject constructor(
     val repository: JobFinderRepository
 ) {
     suspend operator fun invoke(education: Education){
+        validateEducation(education)
         return repository.updateEducation(education)
+    }
+
+    private fun validateEducation(education: Education){
+        validateField(education.degree)
+        validateField(education.school)
+        validateField(education.startDate)
+        validateField(education.city)
+        validateField(education.endDate)
+    }
+
+    private fun validateField(field: String?){
+        if (!field.isNullOrEmpty()){
+            return
+        }else{
+            throw Throwable("Fields can not be empty")
+        }
     }
 }
