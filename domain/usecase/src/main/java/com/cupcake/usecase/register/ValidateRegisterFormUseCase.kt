80532c73f1aea4f3124f37cad32d366e71a18dc4
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2023-06-27 21:09:11 +0300

    solve register validation info

--- domain/usecase/src/main/java/com/cupcake/usecase/register/ValidateRegisterFormUseCase.kt
+++ domain/usecase/src/main/java/com/cupcake/usecase/register/ValidateRegisterFormUseCase.kt
@@ -3,6 +3,7 @@ package com.cupcake.usecase.register
 import com.cupcake.usecase.validation.ValidateConfirmedPasswordUseCase
 import com.cupcake.usecase.validation.ValidateEmailUseCase
 import com.cupcake.usecase.validation.ValidateFullNameUseCase
+import com.cupcake.usecase.validation.ValidateJobTitleUseCase
 import com.cupcake.usecase.validation.ValidatePasswordUseCase
 import com.cupcake.usecase.validation.ValidateUsernameUseCase
 import javax.inject.Inject
@@ -10,6 +11,7 @@ import javax.inject.Inject
 class ValidateRegisterFormUseCase @Inject constructor(
     private val validateFullName: ValidateFullNameUseCase,
     private val validateUsername: ValidateUsernameUseCase,
+    private val validateJobTitle: ValidateJobTitleUseCase,
     private val validateEmail: ValidateEmailUseCase,
     private val validatePassword: ValidatePasswordUseCase,
     private val validateConfirmedPassword: ValidateConfirmedPasswordUseCase,
@@ -18,11 +20,13 @@ class ValidateRegisterFormUseCase @Inject constructor(
         fullName: String,
         userName: String,
         email: String,
+        jobTitle: String,
         password: String,
         confirmedPassword: String
     ): Boolean {
         return validateFullName(fullName).isValid &&
                 validateUsername(userName).isValid &&
+                validateJobTitle(jobTitle).isValid &&
                 validateEmail(email).isValid &&
                 validatePassword(password).isValid &&
                 validateConfirmedPassword(confirmedPassword, password).isValid
--- domain/usecase/src/main/java/com/cupcake/usecase/validation/ValidateJobTitleUseCase.kt
+++ domain/usecase/src/main/java/com/cupcake/usecase/validation/ValidateJobTitleUseCase.kt
@@ -3,16 +3,16 @@ package com.cupcake.usecase.validation
 import com.cupcake.models.ValidationResult
 import javax.inject.Inject
 
-class ValidateFullNameUseCase @Inject constructor() {
-    operator fun invoke(fullName: String): ValidationResult {
-        return if (fullName.isBlank() || fullName.isEmpty()) {
+class ValidateJobTitleUseCase @Inject constructor() {
+    operator fun invoke(jobTitle: String): ValidationResult {
+        return if (jobTitle.isBlank()) {
             ValidationResult(false, ERROR)
-        } else if (fullName.length < MINIMUM_LENGTH) {
+        } else if (jobTitle.length < MINIMUM_LENGTH) {
             ValidationResult(false, ERROR_LENGTH_SHORT)
-        } else if (fullName.length > MAXIMUM_LENGTH) {
+        } else if (jobTitle.length > MAXIMUM_LENGTH) {
             ValidationResult(false, ERROR_LENGTH_LARGE)
-        } else if (!Regex("^[a-zA-Z\\-]+$").matches(fullName)) {
-            ValidationResult(false, ValidateFullNameUseCase.ERROR_LETTER)
+        } else if (!Regex("^[a-zA-Z/-]+$").matches(jobTitle)) {
+            ValidationResult(false, ERROR_LETTER)
         } else {
             ValidationResult(true, "")
         }
@@ -20,11 +20,11 @@ class ValidateFullNameUseCase @Inject constructor() {
 
 
     companion object {
-        private const val MINIMUM_LENGTH = 6
+        private const val MINIMUM_LENGTH = 4
         private const val MAXIMUM_LENGTH = 250
         private const val ERROR_LENGTH_SHORT = "Please write at least 6 length of full name"
         private const val ERROR_LENGTH_LARGE = "Please write at most 250 length of full name"
-        private const val ERROR_LETTER = "The user name must be only letters or dash"
+        private const val ERROR_LETTER = "The job title must contain only letters, dashes, or slashes"
         private const val ERROR = "Please enter a full name"
 
     }
