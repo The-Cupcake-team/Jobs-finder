e4c34285fa2e5b0968271e75386b9244dba1f1bf
David samuel <davidsamuelramsis@gmail.com>
2023-06-29 16:56:10 +0300

    fix empty comment bug

--- presentation/ui/src/main/java/com/cupcake/ui/utill/BindingAdapter.kt
+++ presentation/ui/src/main/java/com/cupcake/ui/utill/BindingAdapter.kt
@@ -3,6 +3,8 @@ package com.cupcake.ui.utill
 
 import android.graphics.drawable.Drawable
 import android.os.Build
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
@@ -230,5 +232,19 @@ fun setSalaryRange(textView: TextView, item: JobUiState) {
     val formattedSalaryRange = formatSalary(minSalary = item.minSalary, maxSalary = item.maxSalary)
     textView.text = formattedSalaryRange
 }
+
+@BindingAdapter("enableOnClickIfTextNotEmpty")
+fun enableOnClickIfTextNotEmpty(view: TextView, editText: EditText) {
+    editText.addTextChangedListener(object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            view.isEnabled = !s.isNullOrEmpty()
+        }
+
+        override fun afterTextChanged(s: Editable?) {}
+    })
+}
+
 
 
