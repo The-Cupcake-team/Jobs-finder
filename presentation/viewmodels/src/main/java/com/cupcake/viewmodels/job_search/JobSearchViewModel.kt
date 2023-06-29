c5dd43c870a294b45acf9d3e17c0727bcca8ff04
HassanAyman <hassan3ayman@gmail.com>
2023-06-29 18:50:06 +0200

    fix some issues in search

--- presentation/viewmodels/src/main/java/com/cupcake/viewmodels/job_search/JobSearchViewModel.kt
+++ presentation/viewmodels/src/main/java/com/cupcake/viewmodels/job_search/JobSearchViewModel.kt
@@ -5,8 +5,10 @@ import com.cupcake.usecase.JobSearchFilterUseCase
 import com.cupcake.viewmodels.base.BaseErrorUiState
 import com.cupcake.viewmodels.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 @HiltViewModel
@@ -25,28 +27,25 @@ class JobSearchViewModel @Inject constructor(
 
     private fun getJobs() {
         _state.update { it.copy(isLoading = true, error = null) }
-        viewModelScope.launch {
-            _state.debounce(1000).collect {
-                tryToExecute(
-                    {
-                        jobSearchFilterUseCase(
-                            searchTitle = it.searchInput,
-                            location = it.jobFilterUIState.location,
-                            jobType = it.jobFilterUIState.jobType,
-                            workType = it.jobFilterUIState.workType,
-                            experienceLevel = it.jobFilterUIState.experience,
-                            salaryRange = Pair(
-                                it.jobFilterUIState.salary.minSalary,
-                                it.jobFilterUIState.salary.maxSalary
-                            )
-                        ).map { it.toJobItemUiState() }
-                    },
-                    ::onSearchJobSuccess,
-                    ::onError
-                )
-            }
-        }
+        tryToExecute(
+            {
+                jobSearchFilterUseCase(
+                    searchTitle = _state.value.searchInput,
+                    location = _state.value.jobFilterUIState.location,
+                    jobType = _state.value.jobFilterUIState.jobType,
+                    workType = _state.value.jobFilterUIState.workType,
+                    experienceLevel = _state.value.jobFilterUIState.experience,
+                    salaryRange = Pair(
+                        _state.value.jobFilterUIState.salary.minSalary,
+                        _state.value.jobFilterUIState.salary.maxSalary
+                    )
+                ).map { it.toJobItemUiState() }
+            },
+            ::onSearchJobSuccess,
+            ::onError
+        )
     }
+
 
     private fun onSearchJobSuccess(jobs: List<JobItemUiState>) {
         _state.update { it.copy(searchResult = jobs, isLoading = false, error = null) }
@@ -58,7 +57,7 @@ class JobSearchViewModel @Inject constructor(
 
     fun onSearchInputChange(newSearchInput: CharSequence) {
         _state.update { it.copy(searchInput = newSearchInput.toString()) }
-        getJobs()
+       getJobs()
     }
 
     fun initialSearchInput(jobTitle: String) {
@@ -95,6 +94,7 @@ class JobSearchViewModel @Inject constructor(
         viewModelScope.launch {
             _event.emit(SearchJobEvent.OnApplyButtonClicked)
         }
+        getJobs()
     }
 
     fun onClearClicked() {
@@ -109,6 +109,11 @@ class JobSearchViewModel @Inject constructor(
 
     fun onRetryClicked() {
         onApplyClicked()
+    }
+
+    fun setSearchText(jobTitle: String) {
+        _state.update { it.copy(searchInput = jobTitle) }
+        getJobs()
     }
 
 }
